97b2f020f279a271dd7212dbe0226819988a5ef3
PradhaprajS <erpradhap.10@gmail.com>
2021-12-14 19:54:14 +0530

    hi

---
+++ src/org/samples/EmployeeAge.java
@@ -0,0 +1,14 @@
+package org.samples;
+
+public class EmployeeAge {
+
+	public static void main(String[] args) {
+		int a=24, b=20;
+		boolean pradhap =false;
+		boolean raju = true;
+		if ((a<b && pradhap) || raju) {
+			System.out.println("raju is elder brother of pradhap");
+		}
+	}
+
+}
